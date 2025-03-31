134c5a1191d6ba419ccf4fb968fa2c7d7186aba2
dianasanjuan0017 <20230019@uthh.edu.mx>
2025-03-30 20:47:31 -1100

    hola

--- src/pages/client/UserDashboard.jsx
+++ src/pages/client/UserDashboard.jsx
@@ -116,7 +116,7 @@ const UserDashboard = () => {
       setTimeout(() => {
         controlarBombaAgua(false);
         setWaterDispensing(false);
-      }, 5500);
+      }, 5100);
     } else {
       setWaterDispensing(false);
       setActionError("Error al activar la bomba de agua");
